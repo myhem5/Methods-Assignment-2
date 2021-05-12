a17f288c395bcec1c5306e9d76b7da60f1a36e8b
cloehng <81209546+cloehng@users.noreply.github.com>
2021-05-12 16:27:38 -0400

    merging data

--- MethodsAssignment2.java
+++ MethodsAssignment2.java
@@ -5,28 +5,56 @@
 * Description: Benfords Law Assignmnet
 */
 
+import java.util.Scanner; 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.regex.Pattern;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
+
 /* Stringdoc Example
-* @Author - Emily
+* @Author - Emily and Chloe
 * Description:
 *
 * @param :
 * @return :
 */
 
-import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.PrintWriter;
-import java.lang.reflect.Array;
-import java.util.List;
-import java.util.ArrayList;
+class MethodsAssignment2 extends Application{
+    private int total = 0;
+    private int count[] = new int[9];
+    private static double percent1;
+    private static double percent2;
+    private static double percent3;
+    private static double percent4;
+    private static double percent5;
+    private static double percent6;
+    private static double percent7;
+    private static double percent8;
+    private static double percent9;
 
-class MethodsAssignment2 {
+    public void BenfordLawAssignment() {
+        try {
+           benfordsLaw();
+        } catch (FileNotFoundException exception) {
+           System.out.println("File not found");
+        }
+     }
+
     public static void main(String[] args) {
         int digitOccurance = 1;
         salesFile();
         exportFile(digitOccurance);
+
+        // Required by JavaFX
+      launch(args);
+      fraudCheck(percent1, percent2, percent3, percent4, percent5, percent6, percent7, percent8, percent9);
 
     }
 
@@ -94,6 +122,123 @@ class MethodsAssignment2 {
         reader.close();
     }
 
+    /*
+    * @Author - Chloe
+    * Benfords Law
+    *
+    * @param :
+    * @return :
+    */
+   public void benfordsLaw() throws FileNotFoundException {
+    Scanner scanner = new Scanner(new File("Sales.csv")); // reads from the file
+
+    // Read the first line which is "Postal Code,Sales"
+    if (scanner.hasNextLine()) {
+       String firstLine = scanner.nextLine();
+    }
+
+    while (scanner.hasNextLine()) {
+       // line exist
+       String line = scanner.nextLine();
+       // System.out.println(line);
+
+       // Split the line with commas
+       String[] columns = line.split(Pattern.quote(","));
+       String postalCode = columns[0];
+       String sales = columns[1];
+
+       char firstCharacter = sales.charAt(0); // looks at first character and increments the count
+       if (firstCharacter == '1') {
+          count[0]++;
+       } else if (firstCharacter == '2') {
+          count[1]++;
+       } else if (firstCharacter == '3') {
+          count[2]++;
+       } else if (firstCharacter == '4') {
+          count[3]++;
+       } else if (firstCharacter == '5') {
+          count[4]++;
+       } else if (firstCharacter == '6') {
+          count[5]++;
+       } else if (firstCharacter == '7') {
+          count[6]++;
+       } else if (firstCharacter == '8') {
+          count[7]++;
+       } else if (firstCharacter == '9') {
+          count[8]++;
+       }
+
+       total++;  // keeps running total of the number of rows
+
+    }
+ }
+    /*
+    * @Author - Chloe
+    * Benfords Law
+    *
+    * @param :
+    * @return :
+    */
+
+ @Override
+ public void start(Stage stage) {
+    stage.setTitle("Benford's Law Assignment");
+
+    CategoryAxis xAxis = new CategoryAxis();
+    xAxis.setLabel("Digit");
+    NumberAxis yAxis = new NumberAxis("Percent", 0, 50, 5);
+    BarChart<String, Number> barChart = new BarChart<String, Number>(xAxis, yAxis);
+    barChart.setTitle("Figure 1 - Benford's Law Distribution Leading Digit");
+
+    double percent1 = (double) count[0] / (double) total * 100;
+    double percent2 = (double) count[1] / (double) total * 100;
+    double percent3 = (double) count[2] / (double) total * 100;
+    double percent4 = (double) count[3] / (double) total * 100;
+    double percent5 = (double) count[4] / (double) total * 100;
+    double percent6 = (double) count[5] / (double) total * 100;
+    double percent7 = (double) count[6] / (double) total * 100;
+    double percent8 = (double) count[7] / (double) total * 100;
+    double percent9 = (double) count[8] / (double) total * 100;
+
+    XYChart.Series series = new XYChart.Series();
+    series.getData().add(new XYChart.Data("1", percent1));
+    series.getData().add(new XYChart.Data("2", percent2));
+    series.getData().add(new XYChart.Data("3", percent3));
+    series.getData().add(new XYChart.Data("4", percent4));
+    series.getData().add(new XYChart.Data("5", percent5));
+    series.getData().add(new XYChart.Data("6", percent6));
+    series.getData().add(new XYChart.Data("7", percent7));
+    series.getData().add(new XYChart.Data("8", percent8));
+    series.getData().add(new XYChart.Data("9", percent9));
+
+    Scene scene = new Scene(barChart, 800, 600);
+    barChart.getData().addAll(series);
+    stage.setScene(scene);
+    stage.show();
+
+    double table1 = percent1;
+
+    return;
+ }
+
+    /*
+    * @Author - Chloe
+    * Benfords Law
+    *
+    * @param :
+    * @return :
+    */
+
+ public static double fraudCheck(double percent1, double percent22, double percent32, double percent42, double percent52, double percent62, double percent72, double percent82, double percent92) {
+     if (percent1 <= 32 && percent1 >= 29) {
+         System.out.println("Fraud Liekly Did Not Occur");
+    } else {
+        System.out.println("Fraud Likely Did Occur");
+    }
+    return percent1;
+ }
+
+
     /* 
      * @Author - Emily
      * export the file as results.csv
@@ -101,7 +246,7 @@ class MethodsAssignment2 {
      * @param : data from code
      * @return : results.cs
      */
-    public static void exportFile(int[] digitOccurance) {
+    public static void exportFile(int digitOccurance) {
         double digitPerc = 0;
         try{
             File export = newFile("results.csv");
@@ -109,9 +254,15 @@ class MethodsAssignment2 {
 
             //Generating CSV
             if(export.exists()){
-                for(int i = 1; i< digitOccurrences.length; i++);
-
-                // Chloe part 
+                out.println("1 = " + percent1 + "%");
+                out.println("2 = " + percent2 + "%");
+                out.println("3 = " + percent3 + "%");
+                out.println("4 = " + percent4 + "%");
+                out.println("5 = " + percent5 + "%");
+                out.println("6 = " + percent6 + "%");
+                out.println("7 = " + percent7 + "%");
+                out.println("8 = " + percent8 + "%");
+                out.println("9 = " + percent9 + "%");
             }
         }
 
